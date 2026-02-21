ee1db4312ebf4cb7895f3d0e317eb9c57f3975fc
Drake-Zerep <drakezerep@gmail.com>
2026-02-21 12:35:52 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
 fetch("data/noticias.json")
-
-.then(response=>response.json())
-
+.then(res=>res.json())
 .then(data=>{
 
+const featuredContainer=document.getElementById("featured-container");
 const newsContainer=document.getElementById("news-container");
 
-const featuredContainer=document.getElementById("featured-container");
-
 data.forEach(news=>{
+
+if(news.featured){
+
+featuredContainer.style.backgroundImage=`url(${news.image})`;
+
+featuredContainer.innerHTML=`
+
+<div class="featured-overlay">
+
+<h2>${news.title}</h2>
+
+<p>${news.summary}</p>
+
+<small>${news.date} · ${news.category}</small>
+
+</div>
+
+`;
+
+}else{
 
 const card=document.createElement("div");
 
 card.className="card";
 
-card.onclick=()=>window.location=news.url;
-
 card.innerHTML=`
+
+<img src="${news.image}" style="width:100%">
 
 <h3>${news.title}</h3>
 
 <p>${news.summary}</p>
 
-<small>${news.date}</small>
+<small>${news.date} · ${news.category}</small>
 
 `;
 
-if(news.featured){
-
-featuredContainer.appendChild(card);
-
-}else{
+card.onclick=()=>location.href=news.url;
 
 newsContainer.appendChild(card);
 
