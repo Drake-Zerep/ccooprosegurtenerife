9cb6871fe01e39eb2f08cd66117d99aa39e9ab56
Drake-Zerep <drakezerep@gmail.com>
2026-02-21 12:39:34 +0000

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,24 @@
+function generate(){
 
+const title=document.getElementById("title").value;
+const summary=document.getElementById("summary").value;
+const image=document.getElementById("image").value;
+const category=document.getElementById("category").value;
+
+const json=
+
+{
+title,
+date:new Date().toLocaleDateString(),
+summary,
+url:"",
+image,
+category,
+featured:false
+};
+
+document.getElementById("output").innerText=
+
+JSON.stringify(json,null,2);
+
+}
